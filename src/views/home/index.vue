e5880028b95c3ad4c482da9661d19e652a421d92
s-yonggangm <1944675778@qq.com>
2025-02-01 13:43:58 +0800

    feat 首页优化

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,38 +1,38 @@
 <template>
   <div class="content-wrapper" ref="canvasWrapperRef">
-    <canvas ref="canvasRef"></canvas>
+    <!-- <canvas ref="canvasRef"></canvas> -->
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import GlslCanvas from 'GlslCanvas';
-import glslCode from './glsl-code.ts';
+// import { ref, onMounted } from 'vue';
+// import GlslCanvas from 'GlslCanvas';
+// import glslCode from './glsl-code.ts';
 
-const canvasRef = ref<HTMLElement | null>(null);
-const canvasWrapperRef = ref<HTMLElement | any>(null);
+// const canvasRef = ref<HTMLElement | null>(null);
+// const canvasWrapperRef = ref<HTMLElement | any>(null);
 
-let canvasBox: any;
+// let canvasBox: any;
 
-function init() {
-  const canvas: any = canvasRef.value;
-  // const width = canvasWrapperRef.value.width;
-  const width = canvasWrapperRef.value.offsetWidth;
-  const height = canvasWrapperRef.value.offsetHeight;
-  canvas.width = width;
-  canvas.height = height;
-  canvasBox = new GlslCanvas(canvas);
-  canvasBox.load(glslCode);
-}
+// function init() {
+//   const canvas: any = canvasRef.value;
+//   // const width = canvasWrapperRef.value.width;
+//   const width = canvasWrapperRef.value.offsetWidth;
+//   const height = canvasWrapperRef.value.offsetHeight;
+//   canvas.width = width;
+//   canvas.height = height;
+//   canvasBox = new GlslCanvas(canvas);
+//   canvasBox.load(glslCode);
+// }
 
-window.addEventListener('resize', () => {
-  canvasBox.destroy();
-  init();
-});
+// window.addEventListener('resize', () => {
+//   canvasBox.destroy();
+//   init();
+// });
 
-onMounted(() => {
-  init();
-});
+// onMounted(() => {
+//   init();
+// });
 </script>
 
 <style scoped>
